e9bbb860d07f73cd28efb0286bb9076acee3a25d
aidar <sharipov_ai@auca.kg>
2023-04-05 23:48:04 +0600

    Created to text fields and added image on envelope

--- Neobis_IOS_LoginApp/ViewController.swift
+++ Neobis_IOS_LoginApp/ViewController.swift
@@ -7,11 +7,28 @@
 // Creating a github repo
 import UIKit
 
+@IBDesignable
+
+class RoundedCornerView: UIView {
+
+    // if cornerRadius variable is set/changed, change the corner radius of the UIView
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LoginTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        LoginTextField.layer.cornerRadius = 30.0
     }
 
 
