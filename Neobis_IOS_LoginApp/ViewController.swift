92e53f51aa1953cecc9f0255cdf87b72b35dbdad
aidar <sharipov_ai@auca.kg>
2023-04-06 16:07:05 +0600

    Done with constraints for iPhone

--- Neobis_IOS_LoginApp/ViewController.swift
+++ Neobis_IOS_LoginApp/ViewController.swift
@@ -22,29 +22,38 @@ class RoundedCornerView: UIView {
 }
 
 class ViewController: UIViewController {
+    @IBAction func ButtonAction(_ sender: UIButton) {
+        print(LoginTextField.text ?? "Username")
+        print(PasswordTextField.text ?? "Password")
+    }
+    
     @IBOutlet weak var SignInButton: UIButton!
+    
     @IBOutlet weak var PasswordTextField: UITextField!
     
     @IBOutlet weak var LoginTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         LoginTextField.layer.cornerRadius = 25.0
         SignInButton.layer.cornerRadius = 25.0
+        
+        
 //        LoginTextField.placeholder = "Username"
         let placeholderText = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
         LoginTextField.attributedPlaceholder = placeholderText
         let placeholderTextPassword = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
         PasswordTextField.attributedPlaceholder = placeholderTextPassword
+        
+        
         LoginTextField.textAlignment = .center
         PasswordTextField.textAlignment = .center
         LoginTextField.textColor = .white
         PasswordTextField.isSecureTextEntry = true
         PasswordTextField.textColor = .white
+        
 
     }
-
-
 }
-
